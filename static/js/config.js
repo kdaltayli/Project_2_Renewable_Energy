c9f7d59208e11b30019ce46e3fb4147e8b8fc2fe
Kursad Altayli <kdaltayli@gmail.com>
2020-11-01 18:05:11 -0500

    Last Update

--- static/js/config.js
+++ static/js/config.js
@@ -1,3 +1,3 @@
 // API key
-const API_KEY = "pk.eyJ1Ijoia3Vyc2FkMTIzIiwiYSI6ImNrZm93YmM5dTBkdjIyeG9mdnl1MGdqbDIifQ.fOkSGw7ARlg01Krcs6aNkA"
+const API_KEY = "PLEASE ENTER YOUR API KEY FOR LEAFLET!"
 
